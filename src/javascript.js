9eaa50f3e50b926bcdd94a1a90937a72edc85c7e
Antônio Gabriel de Matos <antoniobarbarian@gmail.com>
2024-09-22 23:15:06 -0300

    Atualização 22-09: Barra Esquerda e Nav da página do Marketplace, reorganização dos scripts;

---
+++ src/javascript.js
@@ -0,0 +1,12 @@
+function openLoginPage() {
+    window.open('login.html', '_self'); // Abre a página de Login
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Captura o evento de envio do formulário
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Impede o envio do formulário
+        window.location.href = "/MarketPesca/marketplace.html"; // Redireciona para a página desejada
+    });
+});
+
